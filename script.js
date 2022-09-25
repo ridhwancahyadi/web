e8ee62f44ec66e11ba4d67c676484fe0d2dba359
Ridhwan Cahyadi <103169719+ridhwancahyadi@users.noreply.github.com>
2022-09-26 05:44:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
+
+const courseContainers = [...document.querySelectorAll(".course-container")];
+const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
+const preBtn = [...document.querySelectorAll(".pre-btn")];
+
+courseContainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
+  });
+
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
